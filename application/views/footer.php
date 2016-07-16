f6d8a35d4d0b1c8060d528bc7d3dfee8a1c5525a
Pablo Barria <pbarria.reyes@gmail.com>
2016-07-15 23:21:15 -0400

    Bootstrap y login form layout

---
+++ application/views/footer.php
@@ -0,0 +1,6 @@
+<BR>
+<div class="animated fadeInUp">
+<center>&copy 2016 Titan Development Solutions - Todos los derechos reservados</center>
+</div>
+</body>
+</html>
---
+++ application/views/librerias.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="<?php echo base_url();?>plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">   
+    <link href="<?php echo base_url();?>plugins/animate/animate.css" rel="stylesheet"> 
+    <script src="<?php echo base_url();?>plugins/jquery/jquery-2.2.4.js"></script>
+    <script src="<?php echo base_url();?>plugins/jquery/jquery-2.2.4.min.js"></script>
+    <script src="<?php echo base_url();?>plugins/bootstrap/js/bootstrap.js"></script>
+    <script src="<?php echo base_url();?>plugins/bootstrap/js/bootstrap.min.js"></script>
+</head>
